4d83ae56305e7b9c355578f8e6c6dd3e4a7af625
Md Rakib Ali <rakib2020.tkg@gmail.com>
2026-01-13 16:57:56 +0600

    fix: Update RevenueChart component

--- src/pages/Main/RevenueChart.tsx
+++ src/pages/Main/RevenueChart.tsx
@@ -44,7 +44,7 @@ const RevenueChart = () => {
         </div>
       </CardHeader>
       <CardContent>
-        <div className="h-[250px] md:h-[300px] w-full">
+        <div className="h-62.5 md:h-75 w-full">
           <ResponsiveContainer width="100%" height="100%">
             <AreaChart data={data}>
               <defs>
